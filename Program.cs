8713f59a5f72a94ff81491810456c052326c7c03
scanhamman <stevecanham@outlook.com>
2023-02-10 22:50:02 +0100

    First commit from new server

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@
 // Establish a reference to a Monitor repository and pass  
 // both references to a new parameter checker class.
 
-LoggingHelper _logging_helper = new();   
-MonDataLayer _data_layer = new();
-ParameterChecker _param_checker = new (_logging_helper, _data_layer);
+LoggingHelper loggingHelper = new();   
+MonDataLayer dataLayer = new();
+ParameterChecker paramChecker = new (loggingHelper, dataLayer);
 
 // The parameter checker first checks if the program's arguments 
 // can be parsed and if they can then checks if they are valid.
 // If both tests are passed the object returned includes the original arguments.
 
-ParamsCheckResult paramsCheck = _param_checker.CheckParams(args);
+ParamsCheckResult paramsCheck = paramChecker.CheckParams(args);
 if (paramsCheck.ParseError || paramsCheck.ValidityError)
 {
     // End program, parameter errors should have been logged
@@ -26,18 +26,18 @@ else
     {
         // Should be able to proceed - (opts is known to be non-null).
         // Instantiate the relevant working class and call the main 
-        // zipping or unzipping function passingthe command line arguments.
+        // zipping or unzipping function passing the command line arguments.
 
         var opts = paramsCheck.Pars!;     
         if (opts.DoZip)
         {
-            Zipper zipper = new(_logging_helper, _data_layer);
+            Zipper zipper = new(loggingHelper, dataLayer);
             zipper.ZipFiles(opts);
         }
         if (opts.DoUnzip)
         {
-            Unzipper unzipper = new(_logging_helper, _data_layer);
-            unzipper.UnZipFiles(opts);
+            Unzipper un_zipper = new(loggingHelper, dataLayer);
+            un_zipper.UnZipFiles(opts);
         }
 
         return 0;
@@ -46,9 +46,9 @@ else
     {
         // If an error bubbles up to here there is an issue with the code.
 
-        _logging_helper.LogHeader("UNHANDLED EXCEPTION");
-        _logging_helper.LogCodeError("MDR_Zipper application aborted", e.Message, e.StackTrace);
-        _logging_helper.CloseLog();
+        loggingHelper.LogHeader("UNHANDLED EXCEPTION");
+        loggingHelper.LogCodeError("MDR_Zipper application aborted", e.Message, e.StackTrace);
+        loggingHelper.CloseLog();
 
         return -1;
     }
